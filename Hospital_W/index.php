e47629e93942414dbfdfe75254f758c252bf31b2
Jorgelh <jorgeluislh@gmail.com>
2013-08-13 19:42:19 -0600

    s
    s

--- Hospital_W/index.php
+++ Hospital_W/index.php
@@ -1,3 +1,20 @@
-<?
-	phpinfo();
-?>
+.NET Dashboards, your way. Get elegant and interactive data analysis for Windows, the Web and Tablets. Get your FREE 30-day trial today!
+Sponsored
+Learn New Skills That Could Help You Build Your Career. 
+Find a tech program that could teach the skills you may need to achieve your next career goal. Go >>>
+Sponsored By Computer Training Schools
+Post a comment
+Email Article
+Print Article
+ Share ArticlesShare articles
+PHP Tutorial: First Page
+By Vince Barnes
+ This very first page you make that includes some PHP code deserves to be something really useful.  Don't let the simplicity of this little page fool you!  This is a very useful tool -- as you will see when you try it.   This page is going to verify the availability of PHP on your server, and provide you with a lot of information about the PHP itself and the server it's running on.  Here's the code:
+<html>
+<head>
+<title>PHP Test</title>
+</head>
+<body>
+<?php phpinfo(); ?>
+</body>
+</html>
