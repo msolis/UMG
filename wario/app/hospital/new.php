42cc7878564f1187f6c3420c2730091e5e5f2668
Mario Solis <solis.mario91@gmail.com>
2013-11-07 23:03:03 -0600

    nuevo hospital. web services para departamentos filtrados por pais o sin filtro.

--- wario/app/hospital/new.php
+++ wario/app/hospital/new.php
@@ -2,65 +2,51 @@
 	include("../includes/head.php");
 	include("../includes/menu.php");
 	include("../includes/conexion.php");
-	
-	
-	
 ?>
 
 <?php 
 
 	$error = "";
 	$success = "";
+	
 	if (isset($_POST["boton"])){
+		
 		$Nombre = $_POST["Nombre"];
 		$Direccion = $_POST["Direccion"];
 		$Nit = $_POST["Nit"];
-		$Razon = $_POST["Razon"];
+		$RazonSocial = $_POST["RazonSocial"];
 		$Pais = $_POST["Pais"];
 		$Departamento = $_POST["Departamento"];
-		$Sendmail = $_POST["Sendmail"];
+		/*$Sendmail = $_POST["Sendmail"];
 		$Smtp = $_POST["Smtp"];
-		$passmail = $_POST["passmail"];
-		$Smtpport = $_POST["Smtpport"];
+		$Sendmail_password = $_POST["Sendmail_password"];
+		$Smtp_port = $_POST["Smtp_port"];*/
 		$Estatus = $_POST["Estatus"];
-		$Ventacorrelativo = $_POST["Ventacorrelativo "];
-		$Ventacorrelativoauto = $_POST["Ventacorrelativoauto"];
-		$Ventaserie = $_POST["Ventaserie"];
-		$Id= $_POST["Id"];
-		$stmt = $db->prepare("SELECT corporacion, id from hospital where corporacion = ? and id = ?;");
-		$stmt->bind_param('is', $USER_CORPORATION, $Id);
+		$Venta_Correlativo = $_POST["Venta_Correlativo"];
+		$Venta_Correlativo_Autonumerico = $_POST["Venta_Correlativo_Autonumerico"];
+		$Venta_Serie = $_POST["Venta_Serie"];
 		
+		$stmt = $db->prepare("SELECT ifnull(max(id),0)+1 as id from hospital where corporacion = ?;");
+		$stmt->bind_param('i', $USER_CORPORATION);
 		$stmt->execute();
 		$result = $stmt->get_result();
-		$rowCount = mysqli_num_rows($result);
+		$rowArray = mysqli_fetch_array($result);
 		
+		$Id = $rowArray["id"];
 		
-		if ($rowCount == 0){
-			
-			/// get new usuer id
-			$stmt = $db->prepare("SELECT ifnull(max(id),0)+1 as id from hospital where corporacion = ?;");
-			$stmt->bind_param('i', $USER_CORPORATION);
-			$stmt->execute();
-			$result = $stmt->get_result();
-			$rowArray = mysqli_fetch_array($result);
-			
-			$Id = $rowArray["Id"];
-			
-			$stmt = $db->prepare("INSERT INTO hospital (corporacion, id, nombre, direccion, nit, razonsocial, pais, departamento, sendmail_from, smtp, sendmail_passwprd, smtp_port, estatus, venta_correlativo, venta_correlativo_autonumerico, ventaserie) values (?, ?, ?, ?, ?, ?, ?, ?, ?,?,?,?,?,?,?,?);");
-			$stmt->bind_param('iissssiisssisiss', $USER_CORPORATION, $Id, $Nombre, $Direccio, $Nit, $Razon, $Pais, $Departamento, $Sendmail,$Smtp,$passmail,$smtpport,$Estatus,$Estatus,Ventacorrelativo,$Ventacorrelativoauto,$Ventaserie);
-			$rc = $stmt->execute();
-			if (!$rc) {
-				$typeError = "error";
-				$textError = "NO se pudo guardar el registro. Error: " . $stmt->error;
-			}else{
-				$typeError = "success";
-				$textError = "Información ha sido guardada exitosamente.";
-			}
-			
+		$stmt = $db->prepare("insert into hospital (corporacion, id, nombre, direccion, nit, razonsocial, pais, departamento, estatus, venta_correlativo, venta_correlativo_autonumerico, venta_serie) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
+		$stmt->bind_param('iissssiisiss', $USER_CORPORATION, $Id, $Nombre, $Direccion, $Nit, $RazonSocial, $Pais, $Departamento, $Estatus, $Venta_Correlativo, $Venta_Correlativo_Autonumerico, $Venta_Serie);
+
+		$rc = $stmt->execute();
+		if (!$rc) {
+			$typeError = "error";
+			$textError = "NO se pudo guardar el registro. Error: " . $stmt->error;
 		}else{
-			$error = "El Correo electronico ya existe";	
+			$typeError = "success";
+			$textError = "Información ha sido guardada exitosamente.";
 		}
-		echo $error;
+		
+	
 		
 	}
 
@@ -95,30 +81,35 @@
 								</div>
 								<div class="formRow">
 									<label for="nice_text_small">Razon Social</label>
-									<input type="text" id="Razon" name="Razon" class="input-text large">
+									<input type="text" id="RazonSocial" name="RazonSocial" required class="input-text large">
 								</div>
-								<select id="pais" name="pais" class="custom dropdown medium" >
-									<?php newSelector("pais", "id", "descripcion", " where estatus = 'A'", ""); ?>
-								</select>
-								<select id="Departamento" name="Departamento" class="custom dropdown medium" >
-									<?php newSelector("departamento", "id", "descripcion", "where estatus = 'A'" ,""); ?>
-								</select>
 								<div class="formRow">
+									<label for="nice_text_small">Pais</label>
+									<select id="Pais" name="Pais" class="custom dropdown medium" >
+										<?php newSelector("pais", "id", "descripcion", " where estatus = 'A'", ""); ?>
+									</select>
+								</div>
+								<div class="formRow">
+									<label for="nice_text_small">Departamento</label>
+									<select id="Departamento" name="Departamento" class="custom dropdown medium" >
+									</select>
+								</div>
+								<!--div class="formRow">
 									<label for="nice_text">Correo Electronico</label>
-									<input type="text" required id="Sendmail" name="Sendmail" class="input-text">
+									<input type="text" id="Sendmail" name="Sendmail" class="input-text">
 								</div>
 								<div class="formRow">
 									<label for="nice_text">SMTP</label>
-									<input type="text" required id="smtp" name="smtp" class="input-text">
+									<input type="text" id="smtp" name="smtp" class="input-text">
 								</div>
 								<div class="formRow">
 									<label for="nice_text">Contraseña</label>
-									<input type="text" required id="passmail" name="passmail" class="input-text">
+									<input type="text" id="passmail" name="passmail" class="input-text">
 								</div>
 								<div class="formRow">
 									<label for="nice_text">Puerto SMTP</label>
-									<input type="text" required id="Smtpport" name="Smtpport" class="input-text">
-								</div>
+									<input type="text" id="Smtpport" name="Smtpport" class="input-text">
+								</div-->
 								<div class="formRow" style="">
 									<label for="nice_select">Estatus</label>
 									<select id="Estatus" name="Estatus" class="custom dropdown medium" >
@@ -127,20 +118,54 @@
 									</select>
 								</div>
 								<div class="formRow">
-									<label for="nice_text">Correlativo De Venta</label>
-									<input type="text" required id="Ventacorrelativo" name="Ventacorrelativo" class="input-text">
+									<label for="nice_text"># Correlativo De Venta Impreso</label>
+									<input type="text" required id="Venta_Correlativo" name="Venta_Correlativo" class="input-text">
 								</div>
 								<div class="formRow">
-									<label for="nice_text">Auto Numerico</label>
-									<input type="text" required id="Nombre" name="Nombre" class="input-text">
+									<label for="nice_text">Tipo de Correlativo de Facturación</label>
+									<select id="Venta_Correlativo_Autonumerico" name="Venta_Correlativo_Autonumerico" class="custom dropdown medium" >
+										<option value="S">Numero de Factura Autonumerico</option>
+										<option value="N">Numero de Factura Manual</option>
+									</select>
 								</div>
 								<div class="formRow">
-									<label for="nice_text">No de Serie</label>
-									<input type="text" required id="Ventaserie" name="Ventaserie" class="input-text">
+									<label for="nice_text">Serie Para Facturación</label>
+									<input type="text" required id="Venta_Serie" name="Venta_Serie" class="input-text">
 								</div>
-								
-								
+								<div class="formRow">
+                                    <button type="submit" name="boton" class="button small nice blue radius">Guardar</button>
+									<a href="index.php" class="clear_form">Cancelar</a>
+								</div>
 							</form>
+							<script>
+								$(document).on("ready", function(){
+									$("#pais").on("change", function(){
+										change_pais();
+									});
+
+									function change_pais(){
+										$.post("../ws/departamentos.php", { pais: $("#pais").val() }, function(data){
+											$("#Departamento").empty();
+											$( data ).each(function(index, item) {
+												$("#Departamento").append($("<option></option>")
+														         .attr("value",item.id)
+														         .text(item.descripcion));
+											});
+										}, "json");
+									}
+
+									change_pais();
+
+									$("form").on("submit", function(){
+										
+										if (($("#Departamento").val() == "") || ($("#Departamento").val() == null)){
+											alert("Departamento no Valido");
+											return false;
+										}
+										
+									});
+								});
+							</script>
 						</div>
 										
 					</div>
