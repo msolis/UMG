fe6a742bd175fef6491d1274689235f116d15d46
MickeyPorres <rudyporres@gmail.com>
2013-11-08 00:08:28 -0600

    nuevos cambios

--- Hospital_AR/hospital/includes/menu.php
+++ Hospital_AR/hospital/includes/menu.php
@@ -1,6 +1,6 @@
 <ul id="button">
-	<li><a href="usuario.php">Usuarios</a></li>
-	<li><a href=”#”>Clientes</a></li>
+	<li><a href="../Usuario/Usuario.php">Usuarios</a></li>
+	<li><a href="../pacientes/Estado_Paciente.php">Pacientes</a></li>
 	<li><a href=”#”>Servicios</a></li>
 	<li><a href=”#”>Farmacia</a></li>
 	<li><a href=”#”>Laboratorio</a></li>
